512f494e0f674915e7510772235f40c2ee706642
Scotch <horsinaround235@gmail.com>
2022-10-06 17:24:20 -0400

    more fun stuff

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PhotoList from "./PhotoList";
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function Gallery(props) {
+  const currentCategory = {
+    name: "commercial",
+    description:
+      "Photos of grocery stores, food trucks, and other commercial projects",
+  };
+  return (
+    <section>
+      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
+      <PhotoList category={currentCategory.name} />
+    </section>
+  );
+}
+export default Gallery;
